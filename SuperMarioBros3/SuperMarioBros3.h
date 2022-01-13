ce8ab5ba52ff0b22ead526542ee26664825f5acc
Hong Gia Lap <laphg97@gmail.com>
2022-01-13 15:37:04 +0700

    Checkpoint: - World11 Basic Assets

---
+++ SuperMarioBros3/SuperMarioBros3.h
@@ -0,0 +1,31 @@
+#pragma region ACTOR ID
+/* Controller */
+#define ACT_GAMEMASTER			10001
+#define ACT_TITLE				10001
+#define ACT_WORLDMAP			10001
+#define ACT_WORLD				10001
+
+/* Character */
+#define ACT_MARIO				20101
+#define ACT_MARIO_TAIL			20102
+#define ACT_MARIO_FIREBALL		20103
+								
+#define ACT_GOOMBA				20201
+#define ACT_GOOMBA_MICRO		20202
+								
+#define ACT_KOOPA				20301
+#define ACT_KOOPA_SENSOR		20302
+								
+#define ACT_PIRANHA				20401
+#define ACT_PIRANHA_SENSOR		20402
+								
+#define ACT_VENUS				20501
+#define ACT_VENUS_SENSOR		20502
+#define ACT_FIREBALL			20503
+
+/* Item */
+
+/* Prop */
+
+/* Misc */
+#pragma endregion
