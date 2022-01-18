3b0f1871d09f902e2ebc2e9e776355fa992e0557
Hong Gia Lap <laphg97@gmail.com>
2022-01-18 14:50:16 +0700

    Prepping: Goomba Koopa Piranha Venus

--- SuperMarioBros3/SuperMarioBros3.cpp
+++ SuperMarioBros3/SuperMarioBros3.cpp
@@ -10,6 +10,19 @@
 #include "Actor/Character/Mario/MarioTail.h"
 #include "Actor/Character/Mario/MarioFireball.h"
 
+#include "Actor/Character/Goomba/Goomba.h"
+#include "Actor/Character/Goomba/MicroGoomba.h"
+
+#include "Actor/Character/Koopa/Koopa.h"
+#include "Actor/Character/Koopa/KoopaSensor.h"
+
+#include "Actor/Character/Piranha/Piranha.h"
+#include "Actor/Character/Piranha/PiranhaSensor.h"
+
+#include "Actor/Character/Venus/Venus.h"
+#include "Actor/Character/Venus/VenusSensor.h"
+#include "Actor/Character/Venus/Fireball.h"
+
 /* Item */
 /* Prop */
 
@@ -24,10 +37,10 @@ pGameObject CSuperMarioBros3::Create(pScene scene, unsigned int actor, std::stri
 {
 	switch (actor)
 	{
-		/* Controller */
-		
-		/* Character */
-		// Mario
+	/* Controller */
+
+	/* Character */
+	// MARIO
 	case ACT_MARIO:
 	{
 		auto gameObject = new CMario(
@@ -67,8 +80,129 @@ pGameObject CSuperMarioBros3::Create(pScene scene, unsigned int actor, std::stri
 	}
 	break;
 
-		/* Prop */
-		// Platform
+	// GOOMBA
+	case ACT_GOOMBA:
+	{
+		auto gameObject = new CGoomba(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	case ACT_GOOMBA_MICRO:
+	{
+		auto gameObject = new CMicroGoomba(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	// KOOPA
+	case ACT_KOOPA:
+	{
+		auto gameObject = new CKoopa(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	case ACT_KOOPA_SENSOR:
+	{
+		auto gameObject = new CKoopaSensor(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	// PIRANHA
+	case ACT_PIRANHA:
+	{
+		auto gameObject = new CPiranha(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	case ACT_PIRANHA_SENSOR:
+	{
+		auto gameObject = new CPiranhaSensor(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	// VENUS
+	case ACT_VENUS:
+	{
+		auto gameObject = new CVenus(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	case ACT_VENUS_SENSOR:
+	{
+		auto gameObject = new CVenusSensor(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	case ACT_VENUS_FIREBALL:
+	{
+		auto gameObject = new CFireball(
+			this, scene, nextId++,
+			name, source,
+			x, y, gx, gy, layer
+		);
+		Add(gameObject);
+		gameObject->Load();
+		return gameObject;
+	}
+	break;
+
+	/* Prop */
+	// Platform
 	case ACT_PLATFORM:
 	{
 		auto gameObject = new CPlatform(
--- SuperMarioBros3/SuperMarioBros3.h
+++ SuperMarioBros3/SuperMarioBros3.h
@@ -14,6 +14,19 @@
 #define ACT_MARIO_TAIL			20102
 #define ACT_MARIO_FIREBALL		20103
 
+#define ACT_GOOMBA				20201
+#define ACT_GOOMBA_MICRO		20202
+
+#define ACT_KOOPA				20301
+#define ACT_KOOPA_SENSOR		20302
+
+#define ACT_PIRANHA				20401
+#define ACT_PIRANHA_SENSOR		20402
+
+#define ACT_VENUS				20501
+#define ACT_VENUS_SENSOR		20502
+#define ACT_VENUS_FIREBALL		20503
+
 /* Item */
 /* Prop */
 
